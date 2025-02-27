ed5068e2c88a4c86027f06df53103a8902bb167a
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2025-02-27 08:53:48 +0200

    Updated home url and upload_file.

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
+import os
+
+import tensorflow as tf
 from flask import Flask, request
 
+from classifire import classify
+
 app = Flask(__name__)
+
+STATIC_FOLDER = "static"
+UPLOAD_FOLDER = "static/uploads/"
+
+cnn_model = tf.keras.models.load_model(STATIC_FOLDER + "/models/" + "save_at_49.keras")
 
 
 @app.route("/")
 def home():
-    return "<p>Hello world!</p>"
+    img_name = "/static/images/cat-dog.jpg"
+    return (''
+            f'<body style="background-image: url({img_name});>'
+            '<p">Hello world!</p>'
+            '')
 
 
 @app.post("/classify")
 def upload_file():
+    file = request.files["image"]
+    upload_image_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(upload_image_path)
+
+    label, prob = classify(cnn_model, upload_image_path)
+
+    prob = round((float(prob) * 100), 2)
+
     return {
-        "label": "*CAT*",
-        "probability": 0.95
+        "label": label,
+        "probability": prob
     }
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(debug=True)
