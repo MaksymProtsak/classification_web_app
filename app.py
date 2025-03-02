898ca7cb28d499cf993f7e2340e60ec7863e0a78
Maksym Protsak <maksym.protsak@gmail.com>
2025-03-02 22:12:59 +0200

    Updated requiremtnts.txt.

--- app.py
+++ app.py
@@ -33,5 +33,6 @@ def upload_file():
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.debug = True
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
